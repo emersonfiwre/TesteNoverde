a44ae8beb2c8467037b0f8bf265515a2a696f67d
emersonfiwre <lima.emerson2009@gmail.com>
2020-12-15 16:59:48 -0300

    Initial commit

---
+++ app/src/main/java/com/emersonfiwre/testenoverde/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.emersonfiwre.testenoverde
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
